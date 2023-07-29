2a6562941798a92fed0fefa1d86a2405f62ccb4d
vlaureano <vlaureano@AdministorsMBP5.lan>
2023-07-29 18:15:31 +0100

    chore: cluster created

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,4 +2,5 @@ resource "google_container_cluster" "my_autopilot_cluster" {
   name     = "linuxtips"
   location = "us-central1" # or your desired region
   enable_autopilot = true
+  project = "linuxtips-394122"
 }
